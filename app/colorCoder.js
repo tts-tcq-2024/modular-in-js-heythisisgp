4ff3e78dba8f570796e398ef2cdb7f09423ee16c
ganeshpandi <heythisisgp@gmail.com>
2024-08-01 23:44:51 +0530

    Update colorCoder.js

--- app/colorCoder.js
+++ app/colorCoder.js
@@ -1,20 +1,51 @@
-const MajorColorNames = ["WHITE", "RED", "BLACK", "YELLOW", "VIOLET"];
-const MinorColorNames = ["BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"];
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
 
-class ColorPair {
-  constructor(majorColor, minorColor) {
-    this.majorColor = majorColor;
-    this.minorColor = minorColor;
-  }
+function getColorFromPairNumber(pairNumber) {
+    let minorSize = MajorColorNames.length;
+    let majorSize = MinorColorNames.length;
+    
+    if (pairNumber < 1 || pairNumber > minorSize * majorSize) {
+        throw `Argument PairNumber:${pairNumber} is outside the allowed range`;
+    }
+    let zeroBasedPairNumber = pairNumber - 1;
+    let majorIndex = parseInt(zeroBasedPairNumber / minorSize);
+    let minorIndex = parseInt(zeroBasedPairNumber % minorSize);
+    let pair = new (require('./colorPair'))();
+    pair.majorColor = MajorColorNames[majorIndex];
+    pair.minorColor = MinorColorNames[minorIndex];
+    return pair;
+}
 
-  toString() {
-    return `MajorColor:${this.majorColor},MinorColr:${this.minorColor}`;   
+function getPairNumberFromColor(pair) {
+    let majorIndex = -1;
+    for (let i = 0; i < MajorColorNames.length; i++) {
+        if (MajorColorNames[i] == pair.majorColor) {
+            majorIndex = i;
+            break;
+        }
+    }
 
-  }
+    let minorIndex = -1;
+    for (let i = 0; i < MinorColorNames.length; i++) {
+        if (MinorColorNames[i] == pair.minorColor) {
+            minorIndex = i;
+            break;
+        }
+    }
+    
+    if (majorIndex == -1 || minorIndex == -1) {
+        throw `Unknown Colors:${pair.toString()}`;
+    }
+
+    return (majorIndex * MinorColorNames.length) + (minorIndex + 1);
 }
 
 module.exports = {
-  MajorColorNames,
-  MinorColorNames,
-  ColorPair,
+    getColorFromPairNumber,
+    getPairNumberFromColor
 };
